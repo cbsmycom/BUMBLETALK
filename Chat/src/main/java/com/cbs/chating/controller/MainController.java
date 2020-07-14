f79bc8b9e0837556617bcad9aa27640731a97a4c
cbsmycom <jakebox0139@gmail.com>
2020-07-14 17:23:07 +0900

    upload Chat code

---
+++ Chat/src/main/java/com/cbs/chating/controller/MainController.java
@@ -0,0 +1,19 @@
+package com.cbs.chating.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class MainController {
+	
+	@RequestMapping("/chat")
+	public ModelAndView chat() {
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("chat");
+		return mv;
+	}
+	
+	
+
+}
